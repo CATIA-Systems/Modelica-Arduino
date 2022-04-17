d7e735e23b3b2c451da949c705d1d0b1a195f7a3
Torsten Sommer <torsten.sommer@3ds.com>
2022-04-17 14:49:20 +0200

    Use SetEvent() to synchronize threads

--- Arduino/Resources/Source/Arduino/ModelicaArduino.cpp
+++ Arduino/Resources/Source/Arduino/ModelicaArduino.cpp
@@ -1,4 +1,4 @@
-#include <windows.h>
+#include <Windows.h>
 #include <stdio.h>
 #include <conio.h>
 
@@ -14,52 +14,18 @@
 #define INSTANCE SoftArduino::instance
 
 
-DWORD WINAPI MyThreadFunction(LPVOID lpParam) {
-
-	for(;;) {
-		loop();
-	}
-
-	return 0;
-}
-
 /** Constructor function of the external object */
 void * ModelicaArduino_open(void *callbacks) {
 
 	setModelicaUtilityFunctions(reinterpret_cast<ModelicaUtilityFunctions_t *>(callbacks));
-
-	DWORD dwThreadId, dwThrdParam = 1;
-	HANDLE hThread;
 	
-	setup();
- 
-	if (INSTANCE.error) {
-		ModelicaFormatError("Error in setup(): %s", INSTANCE.error);
-	}
-
-	hThread = CreateThread(
-	NULL, 				   // default security attributes
-	0, 					   // use default stack size
-	MyThreadFunction, 	   // thread function
-	&INSTANCE,             // argument to thread function
-	0, 					   // use default creation flags
-	&dwThreadId); 		   // returns the thread identifier
-	 
-	if (hThread == NULL) {
-		ModelicaFormatError("Failed to create arduino thread. %d.\n", GetLastError());
-	}
-
-	return &INSTANCE;
+	return new SoftArduino();
 }
 
 /** Destructor function of the external object */
 void ModelicaArduino_close(void *externalObject) {
-
 	//ModelicaMessage("ModelicaArduino_close()\n");
-
-	// TODO: clean up
-	//if (CloseHandle(hThread) != 0)
-	//	ModelicaFormatMessage("Handle to thread closed successfully.\n");
+	delete INSTANCE;
 }
 
 void ModelicaArduino_update(void *instance__,
@@ -71,38 +37,40 @@ void ModelicaArduino_update(void *instance__,
 							int *pulseWidth,
 							int *pulsePeriod) {
 
-	if (INSTANCE.error) {
-		ModelicaFormatError("Error in loop(): %s", INSTANCE.error);
+	if (INSTANCE->error) {
+		ModelicaFormatError("Error in loop(): %s", INSTANCE->error);
 		return;
 	}
 
 	// update the time
-	INSTANCE.time = static_cast<unsigned long>(time * 1e6);
+	INSTANCE->time = static_cast<unsigned long>(time * 1e6);
 
-	const double vref = INSTANCE.analogReferenceMode == EXTERNAL ? analogReference : 5.;
+	const double V_ref = INSTANCE->analogReferenceMode == EXTERNAL ? analogReference : 5.;
 
 	// TODO: assert vref != 0
 
 	for (int i = 0; i < NUM_ANALOG_INPUTS; i++) {
 		// map analog input from [0,V_ref] to [0,1023]
-		INSTANCE.analog[i] = static_cast<int>((analog[i] / vref) * 1023);
+		INSTANCE->analog[i] = static_cast<int>((analog[i] / V_ref) * 1023);
 	}
+
+	INSTANCE->update();
 
 	for (int i = 0; i < 14; i++) {
 		// map digital input from [0,5] to LOW|HIGH using a threshold of 2.5
 		// TODO: add hysteresis
-		INSTANCE.digital[i] = digital[i] > 2.5 ? HIGH : LOW;
+		INSTANCE->digital[i] = digital[i] > 2.5 ? HIGH : LOW;
 
-		portMode[i]    = INSTANCE.portMode[i];
-		pulseWidth[i]  = INSTANCE.pulseWidth[i];
-		pulsePeriod[i] = INSTANCE.pulsePeriod[i];
+		portMode[i]    = INSTANCE->portMode[i];
+		pulseWidth[i]  = INSTANCE->pulseWidth[i];
+		pulsePeriod[i] = INSTANCE->pulsePeriod[i];
 	}
 
 	// interrupts
-	if (INSTANCE.interruptsEnabled) {
+	if (INSTANCE->interruptsEnabled) {
 		for (int i = 0; i < 2; i++) {
-			if (INSTANCE.interrupts[i]) {
-				INSTANCE.interrupts[i]->update(INSTANCE.digital[interruptToDigitalPin(i)]);
+			if (INSTANCE->interrupts[i]) {
+				INSTANCE->interrupts[i]->update(INSTANCE->digital[interruptToDigitalPin(i)]);
 			}
 		}
 	}
--- Arduino/Resources/Source/Arduino/SoftArduino.h
+++ Arduino/Resources/Source/Arduino/SoftArduino.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Windows.h>
+
 #include "Arduino.h"
 
 typedef void (*InterruptSeriveRoutine)();
@@ -11,7 +13,7 @@ class SoftInterrupt {
 
 public:
 	InterruptSeriveRoutine isr;
-	int mode; // CHANGE = 1, FALLING = 2 or RISING = 3
+	int mode;         // CHANGE = 1, FALLING = 2 or RISING = 3
 	int prePotential; // LOW or HIGH
 
 	explicit SoftInterrupt(InterruptSeriveRoutine isr, int mode, int potential) : isr(isr), mode(mode), prePotential(potential) {}
@@ -27,23 +29,33 @@ public:
 	static const int PORT_MODE_INPUT      = 0;
 	static const int PORT_MODE_DIGITAL    = 1;
 	static const int PORT_MODE_PWM        = 2;
-	static const int DEFAULT_PULSE_PERIOD = 2000;    // default pulse period in microseconds 
+	static const int DEFAULT_PULSE_PERIOD = 2000; // default pulse period in microseconds 
 
-	volatile unsigned long time = 0;                 // current time in micro seconds
-	volatile int analogReferenceMode = DEFAULT;      // analog reference mode [DEFAULT, INTERNAL, INTERNAL1V1 or EXTERNAL]
-	volatile int analog[NUM_ANALOG_INPUTS] = {};     // voltages of the analog pins [0, 255]
-	volatile int digital[NUM_DIGITAL_PINS] = {};     // voltages of the digital pins [0, 255];  TODO: change to "State of digital pins (bool)" 
-	volatile int portMode[NUM_DIGITAL_PINS] = {};	 // 0 = input, 1 = digital, 2 = PWM
-	volatile int pulseWidth[NUM_DIGITAL_PINS] = {};  // pulse width in microseconds
-	volatile int pulsePeriod[NUM_DIGITAL_PINS] = {}; // pulse period in microseconds
+	unsigned long time = 0;                 // current time in micro seconds
+	int analogReferenceMode = DEFAULT;      // analog reference mode [DEFAULT, INTERNAL, INTERNAL1V1 or EXTERNAL]
+	int analog[NUM_ANALOG_INPUTS] = {};     // voltages of the analog pins [0, 255]
+	int digital[NUM_DIGITAL_PINS] = {};     // voltages of the digital pins [0, 255];  TODO: change to "State of digital pins (bool)" 
+	int portMode[NUM_DIGITAL_PINS] = {};	// 0 = input, 1 = digital, 2 = PWM
+	int pulseWidth[NUM_DIGITAL_PINS] = {};  // pulse width in microseconds
+	int pulsePeriod[NUM_DIGITAL_PINS] = { DEFAULT_PULSE_PERIOD }; // pulse period in microseconds
 
-	volatile bool interruptsEnabled = true;
+	bool interruptsEnabled = true;
 	SoftInterrupt *interrupts[2] = {};
 
 	const char *error = nullptr;
 
-	static SoftArduino instance;
+	static SoftArduino* instance;
+
+	HANDLE thread      = INVALID_HANDLE_VALUE;
+	HANDLE inputReady  = INVALID_HANDLE_VALUE;
+	HANDLE outputReady = INVALID_HANDLE_VALUE;
 
 	explicit SoftArduino();
+
+	~SoftArduino();
+
+	void update();
+
+	static DWORD WINAPI MyThreadFunction(LPVOID lpParam);
 	
 };
